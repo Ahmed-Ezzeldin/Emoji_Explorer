c7a9616a1f19046383bc0b6bfbcb70a000f92f2b
Ahmed-Ezzeldin <ahmed.ezzeldin.ibrahim@gmail.com>
2022-09-09 11:21:21 +0200

    Initial Commit

---
+++ Emoji Explorer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Emoji Explorer
+//
+//  Created by Codemaker on 09/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Emoji Explorer/Emoji_ExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_ExplorerApp.swift
+//  Emoji Explorer
+//
+//  Created by Codemaker on 09/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_ExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
